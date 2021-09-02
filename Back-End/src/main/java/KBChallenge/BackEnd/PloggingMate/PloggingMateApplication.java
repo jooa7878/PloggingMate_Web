f1660bee6dc49e8b49a0dd573008e2e73a02ef73
alexjime <alexji.dev@gmail.com>
2021-09-02 21:32:54 +0900

    chore : Init Project

---
+++ Back-End/src/main/java/KBChallenge/BackEnd/PloggingMate/PloggingMateApplication.java
@@ -0,0 +1,13 @@
+package KBChallenge.BackEnd.PloggingMate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PloggingMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PloggingMateApplication.class, args);
+	}
+
+}
---
+++ Back-End/src/test/java/KBChallenge/BackEnd/PloggingMate/PloggingMateApplicationTests.java
@@ -0,0 +1,13 @@
+package KBChallenge.BackEnd.PloggingMate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PloggingMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
