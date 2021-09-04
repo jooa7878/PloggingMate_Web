679eda19d7eb083bbf1a7d34a638d1443e6a27f7
On <cks3066@naver.com>
2021-09-04 22:11:20 +0900

    chore: pages, router

--- Front-End/src/App.js
+++ Front-End/src/App.js
@@ -1,9 +1,20 @@
-import '../src/scss/App.scss';
+import "./scss/App.scss";
+import React from "react";
+
+import { BrowserRouter, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <BrowserRouter>
+        <Route path="/" exact component={Home} />
+        <Route path="/login" exact component={Login} />
+        <Route path="/signup" exact component={SignUp} />
+      </BrowserRouter>
     </div>
   );
 }
