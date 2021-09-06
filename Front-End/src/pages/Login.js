ebe96b574fb632121c1a38b41d4b2907a9f9df8e
On <62434898+cks3066@users.noreply.github.com>
2021-09-06 17:04:57 +0900

    [#7 ] feat : 로그인 / 회원 가입 폼 구성 (#9)
    * feat : 로그인 / 회원가입 페이지 폼 생성

--- Front-End/src/pages/Login.js
+++ Front-End/src/pages/Login.js
@@ -1,7 +1,43 @@
 import React from "react";
+import styled from "styled-components";
 
 const Login = (props) => {
-  return <React.Fragment>Login</React.Fragment>;
+  return (
+    <React.Fragment>
+      <Form
+        onSubmit={(event) => {
+          event.preventDefault();
+          console.dir(event.target.id.value);
+          console.dir(event.target.password.value);
+        }}
+      >
+        <Input
+          required
+          type="email"
+          placeholder="아이디를 입력해주세요.."
+          name="id"
+        />
+        <Input
+          required
+          type="password"
+          placeholder="비밀번호를 입력해주세요."
+          name="password"
+        />
+        <Input type="submit"/>
+      </Form>
+    </React.Fragment>
+  );
 };
 
 export default Login;
+
+const Form = styled.form`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const Input = styled.input`
+  margin: 1em;
+`;
