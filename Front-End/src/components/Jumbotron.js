94e9f020e911a01caba14cf67c17c1eb57c26741
ahnsanghyeok <48887925+jooa7878@users.noreply.github.com>
2021-09-09 03:24:55 +0900

    Mainpage (#1)
    * feat : 메인 페이지 NavBar Jumbotron 일부
    
    * feat : 메인페이지 구현 (Navbar, Footer)
    
    * feat : 로고 색상 변경 & 반응형 변경
    
    * feat : logo 색상 변경 & 반응형

--- Front-End/src/components/Jumbotron.js
+++ Front-End/src/components/Jumbotron.js
@@ -1,31 +1,28 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-
-function Jumbotron () {
+function Jumbotron() {
   return (
     <>
-      <Card>
-        Card
-      </Card>
+      <Card>Card</Card>
     </>
-  )
+  );
 }
 
-const Card = styled.div `
+const Card = styled.div`
   border-radius: 10px;
-  border : 1px solid #d3d3d3;
-  margin-left: 40px;
-  width: 600px;
+  border: 1px solid #d3d3d3;
+  margin-top: 100px;
+  width: 800px;
   height: 400px;
-  cursor : pointer;
-  display: flex; 
+  cursor: pointer;
+  display: flex;
   justify-content: center;
   align-items: center;
 
-  @media screen and (max-width :1000px){
-    margin-top : 20px;
+  @media screen and (max-width: 768px) {
+    width: 500px;
   }
-`
+`;
 
 export default Jumbotron;
