78b7bdb00bd5ddf0da7dd4c1f0946282946d2c81
Minsu (Alex) Ji <alexji.dev@gmail.com>
2021-09-14 15:59:18 +0900

    [#17] feat : 미세먼지 조회하기 API (#24)
    - MicrodustDto 추가
    - RestTemplateConfig 추가
    - GetApiService, PostApiService 인터페이스 추가
    - Service 추가
    - Controller 추가
    - Service, Controller 테스트 추가
    resolves #17

--- Back-End/src/main/java/KBChallenge/BackEnd/PloggingMate/configure/response/DataResponse.java
+++ Back-End/src/main/java/KBChallenge/BackEnd/PloggingMate/configure/response/DataResponse.java
@@ -3,10 +3,12 @@ package KBChallenge.BackEnd.PloggingMate.configure.response;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@ToString
 public class DataResponse<T> extends CommonResponse {
 
     private T result;
